f0f9010988197724efc3d65f826236322b16d219
Patricia <prmonday@gmail.com>
2020-11-18 21:32:08 +0000

    2 test case passed

--- lib/take_a_ten_minute_walk.rb
+++ lib/take_a_ten_minute_walk.rb
@@ -1,3 +1,9 @@
-def ten_minute_walk
-  true 
+def ten_minute_walk(input_minutes)
+  if input_minutes.length != 10
+    false
+  elsif input_minutes.count('n') == input_minutes.count('s') && input_minutes.count('e') == input_minutes.count('w')
+    true
+  else
+    false
+  end
 end
--- spec/take_a_ten_minute_walk_spec.rb
+++ spec/take_a_ten_minute_walk_spec.rb
@@ -1,10 +1,14 @@
 require 'take_a_ten_minute_walk'
 
 describe 'ten_minute_walk' do
-  it 'takes an assortment of directions with the letters n, s, e, or w and return ten minutes walking iqual true.'do
-      walk = ten_minute_walk
-    expect(walk).to eq(true)
+  it 'takes an assortment of directions with the letters n, s, e, or w and return false if there are not 10 minutes walk.'do
+      walk = ten_minute_walk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's'])
+      expect(walk).to eq(false)
+  end
+
+    it 'takes the directions letters n, s, e, or w randomly and return true'do
+      walk = ten_minute_walk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])
+      expect(walk).to eq(true)
     end
     
-
-  end
+end
