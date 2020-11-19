67fe029afc41a4dbc4754fb19a74659691880212
Patricia <prmonday@gmail.com>
2020-11-19 08:23:08 +0000

    5 tests cased and description modified

--- spec/take_a_ten_minute_walk_spec.rb
+++ spec/take_a_ten_minute_walk_spec.rb
@@ -1,12 +1,12 @@
 require 'take_a_ten_minute_walk'
 
 describe 'ten_minute_walk' do
-  it 'takes an assortment of directions with the letters n, s, e, or w and return false if there are not 10 minutes walk.'do
-      walk = ten_minute_walk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's'])
+  it 'takes an assortment of directions with the letters n, s, e, or w and return false if there are less than 10 minutes walk.'do
+      walk = ten_minute_walk(['w', 's'])
       expect(walk).to eq(false)
   end
 
-    it 'takes the directions letters n, s, e, or w randomly and return true'do
+    it 'takes an assortment of directions with the letters n, s, e, or w randomly and return true'do
       walk = ten_minute_walk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])
       expect(walk).to eq(true)
     end
@@ -21,7 +21,7 @@ describe 'ten_minute_walk' do
     expect(walk).to eq(false)
  end
  it 'takes the directions letters n, s, e, or w and return false if return to early'do
-  walk = ten_minute_walk(['w', 's'])
+  walk = ten_minute_walk(['w', 's', 'e'])
   expect(walk).to eq(false)
   end
 end
