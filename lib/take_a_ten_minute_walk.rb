5017696d6b378f487749893181e374e52a6ef6e7
Patricia <prmonday@gmail.com>
2020-11-23 11:35:30 +0000

    upadated method name

--- lib/take_a_ten_minute_walk.rb
+++ lib/take_a_ten_minute_walk.rb
@@ -1,4 +1,4 @@
-def ten_minute_walk(input_minutes)
+def ten_minute_walk?(input_minutes)
   if input_minutes.length != 10
     false
   elsif input_minutes.count('n') == input_minutes.count('s') && input_minutes.count('e') == input_minutes.count('w')
--- spec/take_a_ten_minute_walk_spec.rb
+++ spec/take_a_ten_minute_walk_spec.rb
@@ -2,26 +2,26 @@ require 'take_a_ten_minute_walk'
 
 describe 'ten_minute_walk' do
   it 'takes an assortment of directions with the letters n, s, e, or w and return false if there are less than 10 minutes walk.'do
-      walk = ten_minute_walk(['w', 's'])
+      walk = ten_minute_walk?(['w', 's'])
       expect(walk).to eq(false)
   end
 
     it 'takes an assortment of directions with the letters n, s, e, or w randomly and return true'do
-      walk = ten_minute_walk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])
+      walk = ten_minute_walk?(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])
       expect(walk).to eq(true)
     end
 
     it 'takes the directions letters n, s, e, or w and return false if do not return to the starting point'do
-     walk = ten_minute_walk(['w', 's', 'e', 's', 's', 'e', 's', 'w', 'n', 'n'])
+     walk = ten_minute_walk?(['w', 's', 'e', 's', 's', 'e', 's', 'w', 'n', 'n'])
      expect(walk).to eq(false)
    end
 
    it 'takes the directions letters n, s, e, or w and return false if return an empty array'do
-    walk = ten_minute_walk([])
+    walk = ten_minute_walk?([])
     expect(walk).to eq(false)
  end
  it 'takes the directions letters n, s, e, or w and return false if return to early'do
-  walk = ten_minute_walk(['w', 's', 'e'])
+  walk = ten_minute_walk?(['w', 's', 'e'])
   expect(walk).to eq(false)
   end
 end
